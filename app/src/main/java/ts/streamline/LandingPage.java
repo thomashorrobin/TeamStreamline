bfb074a4636185dd64e69c34590ca5946811684a
rWolstenholme <ryanwolstenholme@gmail.com>
2014-09-13 14:46:24 +1200

    Basic camera function

--- app/src/main/java/ts/streamline/LandingPage.java
+++ app/src/main/java/ts/streamline/LandingPage.java
@@ -1,17 +1,29 @@
 package ts.streamline;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
-
+/*
+ * First page the user sees, helps them take a picture
+ */
 public class LandingPage extends Activity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;//This code is used so we know when it's returning an image
+    ImageView preview;
+    boolean imageSet = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+        preview = (ImageView)findViewById(R.id.previewImg);
+        dispatchTakePictureIntent();
     }
 
 
@@ -22,15 +34,35 @@ public class LandingPage extends Activity {
         return true;
     }
 
+    /*
+     * Tells the phone's camera app to take a picture
+     */
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    /*
+     * A picture has been taken
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            preview.setImageBitmap(imageBitmap);
+            imageSet = true;
+        }
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+       return (id == R.id.action_settings)|| super.onOptionsItemSelected(item);
     }
 }

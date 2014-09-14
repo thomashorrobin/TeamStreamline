13ec5b7c2672d3527ca27d500a95102cb63f512b
thomashorrobin <thomasroberthorrobin@gmail.com>
2014-09-14 16:00:07 +1200

    Added some methods

--- app/src/main/java/ts/streamline/TMListing.java
+++ app/src/main/java/ts/streamline/TMListing.java
@@ -4,16 +4,67 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.thoughtworks.xstream.XStream;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
 public class TMListing extends Activity {
+
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tmlisting);
+        tv = (TextView)findViewById(R.id.textview);
+        XStream stream = new XStream();
+        SearchResult sr = null;
+        try {
+            URL url = new URL("https://touch.trademe.co.nz/api/v1/Search/General.xml?search_string=macbook&category=0&page=1&rows=25&return_metadata=true");
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(url.openStream()));
+            String xml = in.readLine();
+            sr = (SearchResult) stream.fromXML(xml);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if(sr != null){
+            for(Listing l: sr.getList()){
+                tv.append(l.getTitle() + "\n");
+            }
+        }
     }
 
+    public static void main(String[] args){
+        XStream stream = new XStream();
+        SearchResult sr = null;
+        try {
+            URL url = new URL("https://touch.trademe.co.nz/api/v1/Search/General.xml?search_string=macbook&category=0&page=1&rows=25&return_metadata=true");
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(url.openStream()));
+            String xml = in.readLine();
+            sr = (SearchResult) stream.fromXML(xml);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if(sr != null){
+            for(Listing l: sr.getList()){
+                System.out.println(l.getTitle());
+            }
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
